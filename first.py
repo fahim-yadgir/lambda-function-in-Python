b77bdc17fe38e4a252b5d41046289736762825d0
Fahim <fahimyadgir07@gmail.com>
2026-04-06 21:13:50 +0530

    frist commit

---
+++ first.py
@@ -0,0 +1,5 @@
+list1 = [1,2,11,22,11,44,55,66,88,44]
+
+result = list(map(lambda x:x*x, list1))
+
+print(result)
---
+++ without_lambda_function.py
@@ -0,0 +1,8 @@
+list1 = [1,2,5,6,8,9,41,1]
+
+result = []
+
+for i in list1:
+    result.append(i*i)
+
+print(result)
